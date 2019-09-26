998061846460eba22ad556bad72056a66f403cb7
donmahallem <donmahallem@users.noreply.github.com>
2019-09-26 11:32:24 +0200

    Require Secret Token for Authorization (#159)
    * added createSecureToken method
    
    * secret variable storage
    
    * working api header
    
    * fix lint fix issue
    
    * use auth middleware for all routes
    
    * added some docs
    
    * added some docs
    
    * fix some lint issues
    
    * fix race condition with the auth header injector

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,6 @@
  * Source https://github.com/donmahallem/TrapezeClientElectron
  */
 
-/*!
- * Source https://github.com/donmahallem/TrapezeApiExpressRoute
- */
-
 import { AppCallback } from "./app";
 import { parseArgs } from "./cli-commands";
 
